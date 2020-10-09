3ec7eb3b0f695e3b8ff1f530acd2b8d1f3866ee9
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-10-09 21:18:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ require('http')
   if (req.url === '/login') return res.end('Alexandr');
   if (req.url === '/node') return res.end(process.versions.node); 
   if (req.url === '/mirror?x=XYZ') return res.end('XYZ'); 
-  if (req.url === '/day') return res.end('09'); 
+  if (req.url === '/day') return res.end(Day); 
   
   res.end('Alexandr Lazarenko');
+  Data = new Date();
+  Day = Data.getData();
 })
 .listen(process.env.PORT);

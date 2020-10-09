1e4f06552ec68b758ca3fe19038243a3d0a83de2
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-10-09 21:15:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ require('http')
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
 };
+  var DD = getDate(); 
   res.writeHead(200, CORS);
   if (req.url === '/v8') return res.end(process.versions.v8);
   if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
   if (req.url === '/login') return res.end('Alexandr');
   if (req.url === '/node') return res.end(process.versions.node); 
   if (req.url === '/mirror?x=XYZ') return res.end('XYZ'); 
-  if (req.url === '/day') return res.end(DD.Data()); 
+  if (req.url === '/day') return res.end(DD); 
   
   res.end('Alexandr Lazarenko');
 })

76b9d64a172a0cb553822d9a369c1691e5234a39
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-07 18:12:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('https')
 .Server((req, res) => {
   
   const CORS = {
-    //'Content-Type': 'text/plain; charset=UTF-8',
+    
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'

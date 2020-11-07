d51abdb86330149516c415c3fc6dc4751b087ea9
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-07 17:54:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ require('http')
 .Server((req, res) => {
   
   const CORS = {
+    'Content-Type': 'text/plain; charset=UTF-8',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
 };
   res.writeHead(200, CORS);
-  if (req.url === '/v8') return res.end(process.versions.v8);
-  if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
-  if (req.url === '/login') return res.end('Alexandr');
-  if (req.url === '/node') return res.end(process.versions.node); 
-  if (req.url === '/mirror?x=XYZ') return res.end('XYZ'); 
-  if (req.url === '/day') return res.end('09'); 
+  if (req.url === '/login/') return res.end('itmo287666');
+  if (req.url === '/sample/') return res.end('function task(x) {return x.Math.pow(x);}');
   
-  res.end('Alexandr Lazarenko');
+  res.end('itmo287666');
 })
 .listen(process.env.PORT);

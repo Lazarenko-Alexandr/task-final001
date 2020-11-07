e91b630e0c7a38fcbcef94e1760ea443d4febece
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-07 18:40:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('http')
     'Content-Type': 'text/plain; charset=UTF-8',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-    //'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
 };
   res.writeHead(200, CORS);
   if (req.url === '/login/') return res.end('itmo287666');

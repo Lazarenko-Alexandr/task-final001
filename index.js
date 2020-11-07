7d2f528a224f30163f160c98e8175d4352b5bbdd
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-07 18:14:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require('http')
 .Server((req, res) => {
   
   const CORS = {
-    
+    'Content-Type': 'text/plain; charset=UTF-8',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+    //'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
 };
   res.writeHead(200, CORS);
   if (req.url === '/login/') return res.end('itmo287666');

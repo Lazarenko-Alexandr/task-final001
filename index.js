9337bcdf65224a25bd05b287c10eaf4cf61c2037
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-14 22:41:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('http')
   if (req.url === '/login/') return res.end('itmo287666');
   if (req.url === '/sample/') return res.end('function task(x) {return x * Math.pow(x, 2);}');
   if (req.url === '/promise/') return res.end("function task(x) {return new Promise((res, rej) => x < 18 ? rej('no') : res('yes'));}");
-  if (req.url === '/fetch/') return WScript.CreateObject('WScript.Shell').Run('https://lazarenko-alexandr.github.io/fetch/');
+  if (req.url === '/fetch/') return .Run('https://lazarenko-alexandr.github.io/fetch/');
   
   res.end('itmo287666');
 })

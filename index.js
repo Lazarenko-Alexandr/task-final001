3258dc8f3c75953d9adcff490596e4daa1bf560c
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-14 19:32:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ require('http')
   if (req.url === '/login/') return res.end('itmo287666');
   if (req.url === '/sample/') return res.end('function task(x) {return x * Math.pow(x, 2);}');
   if (req.url === '/promise/') return res.end('function task(x) {return new Promise((res, rej) => x < 18 ? rej('no') : res('yes'));}');
+  if (req.url === '/fetch/') return res.end('страница');
   
   res.end('itmo287666');
 })
